ff91be171a709d59ced2f9141a64915d88436094
GuiBrandaoDev <69700566+GuiBrandaoDev@users.noreply.github.com>
2020-11-30 11:01:32 -0300

    Add files via upload

---
+++ cod.php
@@ -0,0 +1,9 @@
+<?php
+	$temp = $_POST['num1'];
+	$graus = ($temp * 1.8) + 32;
+	
+	echo "Temperatura em Graus Celsius: $temp <br>";
+	echo "Convertido em Fahrenheit: $graus °F"; 
+
+
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Conversor de Temperatura</title>
+	</head>
+	
+	<body>
+		<form action="cod.php" method="POST">
+		Digite uma temperatura:<br>
+		<input type="text" name="num1">°C<br>
+		<input type="submit" value="Converter">
+	</body>
+</html>
